f2fd176df221d112f7b26823dbe74ca801fe4eee
Mesezi <94804619+Mesezi@users.noreply.github.com>
2023-09-23 18:05:08 +0100

    Update FailurePopUp.jsx

--- src/components/Popup/FailurePopUp.jsx
+++ src/components/Popup/FailurePopUp.jsx
@@ -7,9 +7,9 @@ const FailurePopUp = ({ setMessage }) => {
       className="w-full p-12 max-w-[699px] bg-[##ffffff03]
       border-[1px] rounded-[5px] border-lightPurple flex flex-col gap-5 justify-center text-center relative"
     >
-      <img src="/src/assets/gray star.png" className="absolute twinkle-animation top-[34%] left-[20%] object-contain h-6 md:h-auto" alt="" />
-      <img src="/src/assets/purple star.png" className="absolute twinkle-animation top-[-5%] right-[-5%] object-contain h-6" alt="" />
-      <img src="/src/assets/gradient star.png" className="absolute twinkle-animation bottom-[-5%] right-10 object-contain h-6 md:h-auto" alt="" />
+      <img src="/assets/gray star.png" className="absolute twinkle-animation top-[34%] left-[20%] object-contain h-6 md:h-auto" alt="" />
+      <img src="/assets/purple star.png" className="absolute twinkle-animation top-[-5%] right-[-5%] object-contain h-6" alt="" />
+      <img src="/assets/gradient star.png" className="absolute twinkle-animation bottom-[-5%] right-10 object-contain h-6 md:h-auto" alt="" />
       <h2 className='font-bold text-lg'>Unable to complete request</h2>
       <p>Try again later</p>
       <button
